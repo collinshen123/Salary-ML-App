ef2b1a9b47ee0e326fd9b1f4a914d389818ad24d
collinshen123 <collinshen123@gmail.com>
2023-09-03 16:01:19 -0700

    sklearn test

--- explore_page.py
+++ explore_page.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import pandas as pd
-import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt 
 
 def shortened_categories(catagories, cutoff):
     catagorical_map = {}
