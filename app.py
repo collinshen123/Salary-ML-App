2231bc2da06182a5c126a704720925f52068eec8
collinshen123 <collinshen123@gmail.com>
2023-09-03 17:10:16 -0700

    app changes

--- app.py
+++ app.py
@@ -2,11 +2,11 @@ import streamlit as st
 from predict_page import show_predict_page
 from explore_page import show_explore_page
 
-st.sidebar.selectbox("Explore or Predict", ("ML Prediction", "Explore Data"))
+page = st.sidebar.selectbox("Explore or Predict", ("ML Prediction", "Explore Data"))
 
 
-show_predict_page()
-# if page == "Predict":
-#     show_predict_page()
-# else:
-#     show_explore_page()
+if page == "Predict":
+    show_predict_page()
+else:
+    show_explore_page()
+
