94c4cbe50509b6c880983d0b1f1dc92038d441b9
collinshen123 <collinshen123@gmail.com>
2023-09-06 15:11:55 -0700

    fixed widget

--- app.py
+++ app.py
@@ -1,11 +1,30 @@
 import streamlit as st
 from predict_page import show_predict_page
 from explore_page import show_explore_page
+import requests
+from streamlit_lottie import st_lottie
 
 page = st.sidebar.selectbox("Explore or Predict", ("ML Prediction", "Explore Data"))
 
 
+def load_lottieurl(url: str):    # sourcery skip: assign-if-exp, reintroduce-else
+    r = requests.get(url)
+    if r.status_code != 200:
+        return None
+    return r.json()
+
+lottie_ML = load_lottieurl('https://lottie.host/03043152-81e4-4b9c-9cca-0714082c67d9/IUNSLXauZS.json')
+
+
 if page == "ML Prediction":
+    st_lottie(
+        lottie_ML,
+        speed=1,
+        loop=True,
+        height=300,
+        width=500,
+        key=1
+    )
     show_predict_page()
 else:
     show_explore_page()
--- predict_page.py
+++ predict_page.py
@@ -1,9 +1,7 @@
-import json
 import streamlit as st
 import pickle
 import numpy as np
-import requests
-from streamlit_lottie import st_lottie
+
 
 
 def load_model():
@@ -19,13 +17,7 @@ le_country = data["le_country"]
 le_education = data["le_education"]
 
 
-def load_lottieurl(url: str):    # sourcery skip: assign-if-exp, reintroduce-else
-    r = requests.get(url)
-    if r.status_code != 200:
-        return None
-    return r.json()
 
-lottie_ML = load_lottieurl('https://lottie.host/03043152-81e4-4b9c-9cca-0714082c67d9/IUNSLXauZS.json')
 
 def show_predict_page():  # sourcery skip: extract-method
     st.title("Software Developer Salary Prediction")
@@ -74,5 +66,3 @@ def show_predict_page():  # sourcery skip: extract-method
         salary = regressor.predict(X)
         st.subheader(f"The estimated salary is ${salary[0]:,.2f}")
 
-    
-    st_lottie(lottie_ML, speed=1, height=200, key="initial")
